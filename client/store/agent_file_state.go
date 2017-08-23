403c6825b5138c8ba10c24434a0f825a351f38d6
Yiran Wang <yiran@uber.com>
2017-08-23 16:59:54 -0700

    kraken: Minor fix for file state
    Reviewers: #kraken, evelynl
    
    Reviewed By: #kraken, evelynl
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1134771

--- client/store/agent_file_state.go
+++ client/store/agent_file_state.go
@@ -1,18 +1,11 @@
 package store
 
-import "code.uber.internal/infra/kraken/client/store/base"
-
-var _directoryLookup = make(map[base.FileState]string)
-
-func registerFileState(s base.FileState, d string) {
-	_directoryLookup[s] = d
+// agentFileState implements FileState for managing files on local disk.
+type agentFileState struct {
+	directory string
 }
-
-// agentFileState implements FileState for managing kraken agent files on local disk.
-// It's used by both peer and origin.
-type agentFileState string
 
 // GetDirectory returns corresponding directory of a state.
 func (state agentFileState) GetDirectory() string {
-	return _directoryLookup[state]
+	return state.directory
 }
