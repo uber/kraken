862af769e9a6dd27eb884460bb7eba63d3b5c007
Igor Morozov <igor@uber.com>
2017-10-09 16:05:45 -0700

    Add p2p agent to origin server
    Reviewers: #kraken, codyg
    
    Reviewed By: #kraken, codyg
    
    Subscribers: codyg, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1235999

--- origin/main.go
+++ origin/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
 	xconfig "code.uber.internal/go-common.git/x/config"
+
 	"code.uber.internal/go-common.git/x/log"
+	"code.uber.internal/infra/kraken/lib/peercontext"
 	"code.uber.internal/infra/kraken/lib/store"
+	"code.uber.internal/infra/kraken/lib/torrent"
+	"code.uber.internal/infra/kraken/metrics"
 	"code.uber.internal/infra/kraken/origin/blobserver"
-	"code.uber.internal/infra/kraken/origin/client"
+	blobclient "code.uber.internal/infra/kraken/origin/client"
 	"code.uber.internal/infra/kraken/utils"
 )
 
 func main() {
+	announceIP := flag.String("announce_ip", "", "ip which peer will announce itself as")
+	announcePort := flag.Int("announce_port", 0, "port which peer will announce itself as")
+	flag.Parse()
+
 	var config Config
 	if err := xconfig.Load(&config); err != nil {
 		panic(err)
@@ -22,20 +31,43 @@ func main() {
 	config.Logging.TextFormatter = &formatter
 	log.Configure(&config.Logging, false)
 
+	// The code below initializes and starts P2P scheduler client.
+	pctx, err := peercontext.New(
+		peercontext.PeerIDFactory(config.Torrent.PeerIDFactory), *announceIP, *announcePort)
+	if err != nil {
+		log.Fatalf("Failed to create peer context: %s", err)
+	}
+
+	stats, closer, err := metrics.New(config.Metrics)
+	if err != nil {
+		log.Fatalf("Failed to init metrics: %s", err)
+	}
+	defer closer.Close()
+
+	store, err := store.NewLocalFileStore(&config.LocalStore, true)
+	if err != nil {
+		log.Fatalf("Failed to create local store: %s", err)
+	}
+
+	client, err := torrent.NewSchedulerClient(&config.Torrent, store, stats, pctx)
+	if err != nil {
+		log.Fatalf("Failed to create scheduler client: %s", err)
+		panic(err)
+	}
+	defer client.Close()
+
+	// The code below starts Blob HTTP server.
 	hostname, err := utils.GetLocalIP()
 	if err != nil {
 		log.Fatalf("Error getting local IP: %s", err)
 	}
-	localStore, err := store.NewLocalFileStore(&config.LocalStore, true)
-	if err != nil {
-		log.Fatalf("Error initializing local store: %s", err)
-	}
-	s, err := blobserver.New(config.BlobServer, hostname, localStore, client.NewBlobAPIClient)
+	s, err := blobserver.New(config.BlobServer, hostname, store, blobclient.NewBlobAPIClient)
 	if err != nil {
 		log.Fatalf("Error initializing blob server: %s", err)
 	}
 
 	addr := fmt.Sprintf(":%d", config.Port)
 	log.Info("Starting origin server %s on %s", hostname, addr)
+
 	log.Fatal(http.ListenAndServe(addr, s.Handler()))
 }
