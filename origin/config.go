d48586918758787278038a24f9f6a50eac8c86f7
Igor Morozov <igor@uber.com>
2017-10-09 16:05:45 -0700

    Add p2p agent to origin server
    Reviewers: #kraken, codyg
    
    Reviewed By: #kraken, codyg
    
    Subscribers: codyg, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1235999

--- origin/config.go
+++ origin/config.go
@@ -3,6 +3,8 @@ package main
 import (
 	"code.uber.internal/go-common.git/x/log"
 	"code.uber.internal/infra/kraken/lib/store"
+	"code.uber.internal/infra/kraken/lib/torrent"
+	"code.uber.internal/infra/kraken/metrics"
 	"code.uber.internal/infra/kraken/origin/blobserver"
 )
 
@@ -11,6 +13,8 @@ type Config struct {
 	Logging    log.Configuration
 	Port       int
 	Verbose    bool
-	BlobServer blobserver.Config
-	LocalStore store.Config
+	BlobServer blobserver.Config `yaml:"blobserver"`
+	LocalStore store.Config      `yaml:"store"`
+	Metrics    metrics.Config    `yaml:"metrics"`
+	Torrent    torrent.Config    `yaml:"torrent"`
 }
