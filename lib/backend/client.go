caa6489b7ea4d7a732c9399b575258c821617140
Cody Gibb <codyg@uber.com>
2017-12-27 16:01:14 -0800

    Introduce backend package
    Summary:
    Adds a new package, `backend`, which encapsulates namespace / storage backend
    management.
    
    I deliberately did not include tests since the S3 interface is outside
    our control and should be tested via integration. This diff primarily focuses
    on providing the fundamental interfaces for the pluggable storage backend work.
    
    Reviewers: #kraken, yiran, igor
    
    Reviewed By: #kraken, yiran, igor
    
    Subscribers: yiran, igor, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1413521

---
+++ lib/backend/client.go
@@ -0,0 +1,26 @@
+package backend
+
+import "code.uber.internal/infra/kraken/lib/fileio"
+
+// Uploader reads blobs from src and uploads them to name. Name should be
+// the digest of the blob.
+type Uploader interface {
+	Upload(name string, src fileio.Reader) error
+}
+
+// Downloader downloads blobs under name and writes them to dst. Name should
+// be the digest of the blob.
+type Downloader interface {
+	Download(name string, dst fileio.Writer) error
+}
+
+// Client defines an interface for uploading and downloading blobs to a remote
+// storage backend. The name parameter in these methods should be the digest
+// of the blob being uploaded.
+//
+// Implementations of Client must be thread-safe, since they are cached and
+// used concurrently by Manager.
+type Client interface {
+	Uploader
+	Downloader
+}
