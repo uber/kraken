f901a72da1e27a98390df532907b9564d1f85a25
Cody Gibb <codyg@uber.com>
2018-02-13 13:41:25 -0800

    Clean up origin wbu2 config / make p2p always enabled
    Reviewers: #kraken, evelynl
    
    Reviewed By: #kraken, evelynl
    
    Subscribers: evelynl, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1502893

--- lib/torrent/config.go
+++ lib/torrent/config.go
@@ -9,7 +9,6 @@ import (
 type Config struct {
 	PeerIDFactory string              `yaml:"peer_id_factory"`
 	PieceLength   int64               `yaml:"piece_length"`
-	Enabled       bool                `yaml:"enabled"`
 	Scheduler     scheduler.Config    `yaml:"scheduler"`
 	NetworkEvent  networkevent.Config `yaml:"network_event"`
 }
