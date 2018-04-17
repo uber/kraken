c286efe064adfc5fd79b4a12e8de54eabbe62a3b
Cody Gibb <codyg@uber.com>
2018-04-17 11:14:57 +0200

    Make agent download endpoint synchronous
    Summary:
    Instead of the agent download endpoint being poll-based and requiring clients to handle backoff,
    make the endpoint syncronous where the endpoint blocks until the file is ready to stream.
    
    Reviewers: #kraken, yiran
    
    Reviewed By: #kraken, yiran
    
    Subscribers: yiran, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1660877

--- agent/agentserver/client.go
+++ agent/agentserver/client.go
@@ -4,64 +4,30 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 
-	"code.uber.internal/infra/kraken/utils/backoff"
 	"code.uber.internal/infra/kraken/utils/httputil"
 )
 
 // Client provides a wrapper for HTTP operations on an agent.
 type Client struct {
-	backoff *backoff.Backoff
-	addr    string
-}
-
-type clientOpts struct {
-	timeout time.Duration
-}
-
-// ClientOption defines an option for the Client.
-type ClientOption func(*clientOpts)
-
-// WithTimeout sets timeout t for Client downloads.
-func WithTimeout(t time.Duration) ClientOption {
-	return func(o *clientOpts) { o.timeout = t }
+	addr string
 }
 
 // NewClient creates a new client for an agent at addr.
-func NewClient(addr string, opts ...ClientOption) *Client {
-
-	settings := &clientOpts{
-		timeout: 15 * time.Minute,
-	}
-	for _, opt := range opts {
-		opt(settings)
-	}
-
-	b := backoff.New(backoff.Config{
-		RetryTimeout: settings.timeout,
-	})
-
-	return &Client{b, addr}
+func NewClient(addr string) *Client {
+	return &Client{addr}
 }
 
 // Download returns the blob for namespace / name. Callers should close the
 // returned ReadCloser when done reading the blob.
 func (c *Client) Download(namespace, name string) (io.ReadCloser, error) {
-	a := c.backoff.Attempts()
-	for a.WaitForNext() {
-		resp, err := httputil.Get(
-			fmt.Sprintf("http://%s/namespace/%s/blobs/%s", c.addr, namespace, name),
-			httputil.SendAcceptedCodes(http.StatusOK, http.StatusAccepted))
-		if err != nil {
-			return nil, err
-		}
-		if resp.StatusCode == http.StatusAccepted {
-			continue
-		}
-		return resp.Body, nil
+	resp, err := httputil.Get(
+		fmt.Sprintf("http://%s/namespace/%s/blobs/%s", c.addr, namespace, name),
+		httputil.SendAcceptedCodes(http.StatusOK, http.StatusAccepted))
+	if err != nil {
+		return nil, err
 	}
-	return nil, a.Err()
+	return resp.Body, nil
 }
 
 // Delete deletes the torrent for name.
