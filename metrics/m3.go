64e424306a2063560f5cf879fc34061ff8cbad6e
Evelyn Liu <evelynl@uber.com>
2018-05-29 13:49:36 -0700

    Fix build-index m3 metrics
    Summary: Just realized that build-index is missing m3 config so metrics wont show up...
    
    Test Plan:
    make test
    make integration
    
    Reviewers: #kraken, codyg
    
    Reviewed By: #kraken, codyg
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1777401

--- metrics/m3.go
+++ metrics/m3.go
@@ -13,6 +13,16 @@ func newM3Scope(config Config, cluster string) (tally.Scope, io.Closer, error) {
 	if cluster == "" {
 		return nil, nil, fmt.Errorf("cluster required for m3")
 	}
+
+	if config.M3.Service == "" {
+		return nil, nil, fmt.Errorf("service required for m3")
+	}
+
+	// HostPort is required for m3 metrics but tally/m3 does not fail when HostPort is empty.
+	if config.M3.HostPort == "" {
+		return nil, nil, fmt.Errorf("host_port required for m3")
+	}
+
 	m3Config := m3.Configuration{
 		HostPort: config.M3.HostPort,
 		Service:  config.M3.Service,
