404b54b9d4083697353b029d601ff385df3583ee
Cody Gibb <codyg@uber.com>
2018-06-15 15:49:11 -0700

    Revise download time buckets
    Summary:
    The current download time buckets are fairly useless because they lump all blobs > 1GB together.
    We don't have any visibility into the interesting / problematic downloads of large blobs.
    
    This diff shifts the ranges like so:
    ```
    xsmall: [0, 100MB)
    small:  [100MB, 1GB)
    medium: [1GB, 2GB)
    large:  [2GB, 5GB)
    xlarge : [5GB, 10GB)
    xxlarge: [10GB, ∞)
    ```
    
    Furthermore, the current metrics use a single duration histogram for all sizes, which has high
    granularity for short download times and low granularity for long download times. This makes it
    difficult to measure large blob downloads, which are reduced to huge ranges (e.g. 5-10minutes).
    
    This diff uses a custom histogram for each bucket, in order to maximize granularity for the expected
    download times.
    
    Reviewers: #kraken, evelynl, yiran
    
    Reviewed By: #kraken, evelynl, yiran
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1825755

--- lib/torrent/scheduler/buckets.go
+++ lib/torrent/scheduler/buckets.go
@@ -7,65 +7,92 @@ import (
 	"github.com/uber-go/tally"
 )
 
-var _sizeBuckets = [...]uint64{
-	10 * memsize.MB,
-	100 * memsize.MB,
-	memsize.GB,
-	10 * memsize.GB,
+type bucket struct {
+	name      string
+	min       uint64
+	durations tally.DurationBuckets
 }
 
-// getSizeBucket rounds size up to the nearest bucket.
-func getSizeBucket(size uint64) (b uint64) {
-	for _, b = range _sizeBuckets {
-		if b >= size {
+func newBucket(name string, min uint64) *bucket {
+	return &bucket{name, min, nil}
+}
+
+func (b *bucket) addRange(start, stop, width time.Duration) {
+	cur := start
+	for cur < stop {
+		b.durations = append(b.durations, cur)
+		cur += width
+	}
+}
+
+var _buckets []*bucket
+
+func init() {
+	xs := newBucket("xsmall", 0)
+	xs.addRange(250*time.Millisecond, time.Second, 250*time.Millisecond)
+	xs.addRange(time.Second, 20*time.Second, time.Second)
+	xs.addRange(20*time.Second, time.Minute, 5*time.Second)
+	xs.addRange(time.Minute, 5*time.Minute, time.Minute)
+	xs.addRange(5*time.Minute, 30*time.Minute, 5*time.Minute)
+	_buckets = append(_buckets, xs)
+
+	s := newBucket("small", 100*memsize.MB)
+	s.addRange(time.Second, 30*time.Second, time.Second)
+	s.addRange(30*time.Second, time.Minute, 5*time.Second)
+	s.addRange(time.Minute, 5*time.Minute, time.Minute)
+	s.addRange(5*time.Minute, 30*time.Minute, 5*time.Minute)
+	_buckets = append(_buckets, s)
+
+	m := newBucket("medium", memsize.GB)
+	m.addRange(2*time.Second, time.Minute, 2*time.Second)
+	m.addRange(time.Minute, 2*time.Minute, 5*time.Second)
+	m.addRange(2*time.Minute, 5*time.Minute, 30*time.Second)
+	m.addRange(5*time.Minute, 10*time.Minute, time.Minute)
+	m.addRange(10*time.Minute, 30*time.Minute, 5*time.Minute)
+	_buckets = append(_buckets, m)
+
+	l := newBucket("large", 2*memsize.GB)
+	l.addRange(5*time.Second, 3*time.Minute, 5*time.Second)
+	l.addRange(3*time.Minute, 5*time.Minute, 30*time.Second)
+	l.addRange(5*time.Minute, 10*time.Minute, time.Minute)
+	l.addRange(10*time.Minute, 30*time.Minute, 5*time.Minute)
+	_buckets = append(_buckets, l)
+
+	xl := newBucket("xlarge", 5*memsize.GB)
+	xl.addRange(10*time.Second, 5*time.Minute, 10*time.Second)
+	xl.addRange(5*time.Minute, 10*time.Minute, 30*time.Second)
+	xl.addRange(10*time.Minute, 30*time.Minute, time.Minute)
+	_buckets = append(_buckets, xl)
+
+	xxl := newBucket("xxlarge", 10*memsize.GB)
+	xxl.addRange(15*time.Second, 10*time.Minute, 15*time.Second)
+	xxl.addRange(10*time.Minute, 15*time.Minute, 30*time.Second)
+	xxl.addRange(15*time.Minute, 30*time.Minute, time.Minute)
+	_buckets = append(_buckets, xxl)
+
+	// Sanity check to ensure buckets are sorted.
+	for i := 0; i < len(_buckets)-1; i++ {
+		if _buckets[i].min >= _buckets[i+1].min {
+			panic("buckets are not sorted properly")
+		}
+	}
+}
+
+// getBucket selects the largest bucket size fits into.
+func getBucket(size uint64) (b *bucket) {
+	for i := len(_buckets) - 1; i >= 0; i-- {
+		b = _buckets[i]
+		if size >= b.min {
 			break
 		}
 	}
 	return b
 }
 
-var _durationBuckets = tally.DurationBuckets{
-	100 * time.Millisecond,
-	250 * time.Millisecond,
-	500 * time.Millisecond,
-	750 * time.Millisecond,
-	time.Second,
-	2 * time.Second,
-	3 * time.Second,
-	4 * time.Second,
-	5 * time.Second,
-	6 * time.Second,
-	7 * time.Second,
-	8 * time.Second,
-	9 * time.Second,
-	10 * time.Second,
-	12 * time.Second,
-	14 * time.Second,
-	16 * time.Second,
-	18 * time.Second,
-	20 * time.Second,
-	25 * time.Second,
-	30 * time.Second,
-	35 * time.Second,
-	40 * time.Second,
-	45 * time.Second,
-	50 * time.Second,
-	55 * time.Second,
-	time.Minute,
-	2 * time.Minute,
-	3 * time.Minute,
-	4 * time.Minute,
-	5 * time.Minute,
-	10 * time.Minute,
-	15 * time.Minute,
-	20 * time.Minute,
-	25 * time.Minute,
-	30 * time.Minute,
-}
-
 func recordDownloadTime(stats tally.Scope, size int64, t time.Duration) {
+	b := getBucket(uint64(size))
 	stats.Tagged(map[string]string{
-		"size":    memsize.Format(getSizeBucket(uint64(size))),
-		"version": "2", // Until old stats expire.
-	}).Histogram("download_time", _durationBuckets).RecordDuration(t)
+		"bucket":  b.name,
+		"version": "3",
+	}).Histogram("download_time", b.durations).RecordDuration(t)
 }
--- lib/torrent/scheduler/buckets_test.go
+++ lib/torrent/scheduler/buckets_test.go
@@ -2,27 +2,46 @@ package scheduler
 
 import (
 	"testing"
+	"time"
 
 	"code.uber.internal/infra/kraken/utils/memsize"
+	"github.com/stretchr/testify/require"
+	"github.com/uber-go/tally"
 )
 
-func TestGetSizeBucket(t *testing.T) {
+func TestGetBucket(t *testing.T) {
 	tests := []struct {
 		desc     string
 		size     uint64
-		expected uint64
+		expected string
 	}{
-		{"below min", memsize.KB, 10 * memsize.MB},
-		{"above max", 30 * memsize.GB, 10 * memsize.GB},
-		{"between buckets", 500 * memsize.MB, memsize.GB},
-		{"exact bucket", memsize.GB, memsize.GB},
+		{"below min", memsize.MB, "xsmall"},
+		{"above max", 30 * memsize.GB, "xxlarge"},
+		{"between buckets", 3 * memsize.GB, "large"},
+		{"exact bucket", 1 * memsize.GB, "medium"},
 	}
 	for _, test := range tests {
 		t.Run(test.desc, func(t *testing.T) {
-			result := getSizeBucket(test.size)
-			if test.expected != result {
-				t.Fatalf("Expected %s, got %s", memsize.Format(test.expected), memsize.Format(result))
-			}
+			require.Equal(t, test.expected, getBucket(test.size).name)
+		})
+	}
+}
+
+func TestBucketAddRange(t *testing.T) {
+	tests := []struct {
+		desc               string
+		start, stop, width time.Duration
+		expected           []time.Duration
+	}{
+		{"normal", 1, 4, 1, []time.Duration{1, 2, 3}},
+		{"single", 1, 4, 5, []time.Duration{1}},
+		{"none", 4, 1, 1, nil},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			b := newBucket("test", 0)
+			b.addRange(test.start, test.stop, test.width)
+			require.Equal(t, tally.DurationBuckets(test.expected), b.durations)
 		})
 	}
 }
