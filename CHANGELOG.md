1b8e071280bfa1b071203dc0febf458afee52f3e
Cody Gibb <codyg@uber.com>
2018-06-27 10:32:01 -0700

    Add devcluster image
    Summary:
    Runs all components in a single container. Will be used by TerraBlob team for their
    integration tests.
    
    Note, we will need to add terrablob namespaces once the backend client is landed.
    
    Reviewers: #kraken, yiran
    
    Reviewed By: #kraken, yiran
    
    Subscribers: yiran, jenkins
    
    Differential Revision: https://code.uberinternal.com/D1853951

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+1.0.0 (unreleased)
+====
+
+0.0.0 (2018-06-28)
+====
+- Alpha release for test image (@codyg)
---
+++ docker/devcluster/start.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+baseconfig=/home/udocker/kraken/config
+
+redis-server &
+
+sleep 3
+
+/usr/bin/kraken-testfs \
+    -port=7357 \
+    &>/var/log/udocker/kraken-testfs/stdout.log &
+
+UBER_CONFIG_DIR=$baseconfig/origin /usr/bin/kraken-origin \
+    -blobserver_port=9003 \
+    -peer_ip=localhost \
+    -peer_port=5081 \
+    -config=devcluster.yaml \
+    -zone=devcluster \
+    -cluster=devcluster \
+    &>/var/log/udocker/kraken-origin/stdout.log &
+
+UBER_CONFIG_DIR=$baseconfig/build-index /usr/bin/kraken-build-index \
+    -config=devcluster.yaml \
+    -cluster=devcluster \
+    -port=5263 \
+    &>/var/log/udocker/kraken-build-index/stdout.log &
+
+UBER_CONFIG_DIR=$baseconfig/tracker /usr/bin/kraken-tracker \
+    -config=devcluster.yaml \
+    -cluster=devcluster \
+    &>/var/log/udocker/kraken-tracker/stdout.log &
+
+UBER_CONFIG_DIR=$baseconfig/proxy /usr/bin/kraken-proxy \
+    -config=devcluster.yaml \
+    -cluster=devcluster \
+    &>/var/log/udocker/kraken-proxy/stdout.log &
+
+UBER_CONFIG_DIR=$baseconfig/agent /usr/bin/kraken-agent \
+    -peer_ip=localhost \
+    -peer_port=8988 \
+    -agent_server_port=7602 \
+    -config=devcluster.yaml \
+    -zone=devcluster \
+    -cluster=devcluster \
+    &>/var/log/udocker/kraken-agent/stdout.log &
+
+sleep 3
+
+# Poor man's supervisor.
+while : ; do
+    for c in kraken-testfs kraken-origin kraken-build-index kraken-tracker kraken-proxy kraken-agent; do
+        ps aux | grep $c | grep -q -v grep
+        status=$?
+        if [ $status -ne 0 ]; then
+            echo "$c exited unexpectedly. Logs:"
+            cat /var/log/udocker/$c/stdout.log
+            exit 1
+        fi
+    done
+    sleep 30
+done
