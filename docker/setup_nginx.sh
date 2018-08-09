35819779facb9a9f6deef7d9bfa4dcf09b6d17a8
Cody Gibb <codyg@uber.com>
2018-08-09 14:30:01 -0700

    Move generated nginx confs from /etc to /tmp
    Summary: We should avoid writing anything to /etc.
    
    Reviewers: #kraken, yiran
    
    Reviewed By: #kraken, yiran
    
    Subscribers: jenkins
    
    Differential Revision: https://code.uberinternal.com/D1972711

--- docker/setup_nginx.sh
+++ docker/setup_nginx.sh
@@ -2,11 +2,8 @@
 
 set -ex
 
-for d in /etc/nginx/ /var/lib/nginx/ /var/log/nginx/ /var/run/nginx/; do
+for d in /tmp/nginx/ /var/lib/nginx/ /var/log/nginx/ /var/run/nginx/; do
     mkdir -p $d
     chown -R udocker:udocker $d
     chmod 0755 $d
 done
-
-rm -f /etc/nginx/sites-enabled/default
-rm -f /etc/nginx/sites-available/default
