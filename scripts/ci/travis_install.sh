6b065c74e28f89c8bf7a6a3f29e301f08305a5de
Yiran Wang <yiran@uber.com>
2018-12-18 17:19:03 -0800

    Fix protobuf

---
+++ scripts/ci/travis_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -ex
+wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
+tar -xzvf protobuf-2.4.1.tar.gz
+cd protobuf-2.4.1 && ./configure --prefix=/usr && make && sudo make install
