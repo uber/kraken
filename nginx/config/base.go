cb4b850baa38099c81da11c18335089c7fc538d4
Evelyn Liu <evelynl@uber.com>
2019-03-15 15:16:31 -0700

    still disable msie6

--- nginx/config/base.go
+++ nginx/config/base.go
@@ -125,14 +125,13 @@ http {
   ##
 
   # gzip on;
-  # gzip_disable "msie6";
-
   # gzip_vary on;
   # gzip_proxied any;
   # gzip_comp_level 6;
   # gzip_buffers 16 8k;
   # gzip_http_version 1.1;
 
+  gzip_disable "msie6";
   gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
 
   ##
