f058f2952b82ed36048c01c2c65319ea8e6e5ef0
Cody Gibb <codyg@uber.com>
2020-08-03 17:16:35 -0700

    Update mocks

--- mocks/tracker/peerstore/store.go
+++ mocks/tracker/peerstore/store.go
@@ -33,6 +33,18 @@ func (m *MockStore) EXPECT() *MockStoreMockRecorder {
 	return m.recorder
 }
 
+// Close mocks base method
+func (m *MockStore) Close() {
+	m.ctrl.T.Helper()
+	m.ctrl.Call(m, "Close")
+}
+
+// Close indicates an expected call of Close
+func (mr *MockStoreMockRecorder) Close() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Close", reflect.TypeOf((*MockStore)(nil).Close))
+}
+
 // GetPeers mocks base method
 func (m *MockStore) GetPeers(arg0 core.InfoHash, arg1 int) ([]*core.PeerInfo, error) {
 	m.ctrl.T.Helper()
