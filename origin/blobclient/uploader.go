2ce91690146f225130bba02fa7f9336e60a183f2
Evelyn Liu <evelynl@uber.com>
2020-09-15 16:59:47 -0700

    Add retry uploader

--- origin/blobclient/uploader.go
+++ origin/blobclient/uploader.go
@@ -79,7 +79,8 @@ func newTransferClient(addr string, tls *tls.Config) *transferClient {
 func (c *transferClient) start(d core.Digest) (uid string, err error) {
 	r, err := httputil.Post(
 		fmt.Sprintf("http://%s/internal/blobs/%s/uploads", c.addr, d),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	if err != nil {
 		return "", err
 	}
@@ -99,7 +100,8 @@ func (c *transferClient) patch(
 		httputil.SendHeaders(map[string]string{
 			"Content-Range": fmt.Sprintf("%d-%d", start, stop),
 		}),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	return err
 }
 
@@ -107,7 +109,8 @@ func (c *transferClient) commit(d core.Digest, uid string) error {
 	_, err := httputil.Put(
 		fmt.Sprintf("http://%s/internal/blobs/%s/uploads/%s", c.addr, d, uid),
 		httputil.SendTimeout(15*time.Minute),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	return err
 }
 
@@ -137,7 +140,8 @@ func (c *uploadClient) start(d core.Digest) (uid string, err error) {
 	r, err := httputil.Post(
 		fmt.Sprintf("http://%s/namespace/%s/blobs/%s/uploads",
 			c.addr, url.PathEscape(c.namespace), d),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	if err != nil {
 		return "", err
 	}
@@ -158,7 +162,8 @@ func (c *uploadClient) patch(
 		httputil.SendHeaders(map[string]string{
 			"Content-Range": fmt.Sprintf("%d-%d", start, stop),
 		}),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	return err
 }
 
@@ -187,6 +192,7 @@ func (c *uploadClient) commit(d core.Digest, uid string) error {
 		fmt.Sprintf(template, c.addr, url.PathEscape(c.namespace), d, uid),
 		httputil.SendTimeout(15*time.Minute),
 		httputil.SendBody(body),
-		httputil.SendTLS(c.tls))
+		httputil.SendTLS(c.tls),
+		httputil.SendRetry())
 	return err
 }
