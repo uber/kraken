6ccbd55d98c804e5ed535556f3a8bb38a7732fda
Gaurav Kumar <gauravk@uber.com>
2024-10-01 16:02:44 +0200

    Ran go fmt to fix linting issues

--- lib/backend/constants.go
+++ lib/backend/constants.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	   http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,8 +20,8 @@ import (
 )
 
 const (
-	DefaultPartSize                   int64             = int64(64 * memsize.MB)
-	DefaultBufferGuard                datasize.ByteSize = 10 * datasize.MB
-	DefaultConcurrency                int               = 10
-	DefaultListMaxKeys                int               = 250
+	DefaultPartSize    int64             = int64(64 * memsize.MB)
+	DefaultBufferGuard datasize.ByteSize = 10 * datasize.MB
+	DefaultConcurrency int               = 10
+	DefaultListMaxKeys int               = 250
 )
