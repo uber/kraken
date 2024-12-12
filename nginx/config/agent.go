fa42ec5bab17d54d271bcd126ca717d1017f5e29
akalpakchiev <99887781+Anton-Kalpakchiev@users.noreply.github.com>
2024-12-12 13:42:37 +0200

    Route agent registry readiness API to agent server (#387)

--- nginx/config/agent.go
+++ nginx/config/agent.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -40,7 +40,7 @@ server {
   gzip on;
   gzip_types text/plain test/csv application/json;
 
-  location /health {
+  location ~ ^/(health|readiness)$ {
     proxy_pass http://agent-server;
   }
 
