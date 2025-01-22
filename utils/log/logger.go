cfcda817dd11fae4f5f48823e8a53f942f903600
Zach Cheung <zach.cheung@uber.com>
2025-01-22 11:52:38 -0800

    default time encoder (#391)

--- utils/log/logger.go
+++ utils/log/logger.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,6 +34,9 @@ func (c Config) applyDefaults() Config {
 	}
 	if c.Encoding == "" {
 		c.Encoding = "console"
+	}
+	if c.EncodeTime == nil {
+		c.EncodeTime = zapcore.ISO8601TimeEncoder
 	}
 	return c
 }
