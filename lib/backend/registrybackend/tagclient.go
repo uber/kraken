f32eff951281d0a76ecfd824b2e060c4f48060ec
hweawer <alexandergusakovsky@gmail.com>
2025-01-27 14:32:16 +0100

    Revert HTTPS in registry

--- lib/backend/registrybackend/tagclient.go
+++ lib/backend/registrybackend/tagclient.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -53,7 +53,7 @@ func (f *tagClientFactory) Create(
 	return NewTagClient(config, stats)
 }
 
-const _tagquery = "https://%s/v2/%s/manifests/%s"
+const _tagquery = "http://%s/v2/%s/manifests/%s"
 
 // TagClient stats and downloads tag from registry.
 type TagClient struct {
