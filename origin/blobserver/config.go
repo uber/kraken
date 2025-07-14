d102a2a59a90867e6fc894b7c540fde3eae26376
hweawer <alexandergusakovsky@gmail.com>
2025-07-14 12:50:14 +0200

    Improve kraken origin logging

--- origin/blobserver/config.go
+++ origin/blobserver/config.go
@@ -23,11 +23,61 @@ import (
 type Config struct {
 	Listener                  listener.Config `yaml:"listener"`
 	DuplicateWriteBackStagger time.Duration   `yaml:"duplicate_write_back_stagger"`
+	
+	// Timeout configurations
+	DownloadTimeout       time.Duration `yaml:"download_timeout"`
+	UploadTimeout         time.Duration `yaml:"upload_timeout"`
+	ReplicationTimeout    time.Duration `yaml:"replication_timeout"`
+	BackendTimeout        time.Duration `yaml:"backend_timeout"`
+	ReadinessTimeout      time.Duration `yaml:"readiness_timeout"`
+	
+	// Limit configurations
+	MaxConcurrentDownloads int `yaml:"max_concurrent_downloads"`
+	MaxConcurrentUploads   int `yaml:"max_concurrent_uploads"`
+	MaxRequestSize         int64 `yaml:"max_request_size"`
+	
+	// Retry configurations
+	MaxRetries    int           `yaml:"max_retries"`
+	RetryDelay    time.Duration `yaml:"retry_delay"`
+	RetryMaxDelay time.Duration `yaml:"retry_max_delay"`
 }
 
 func (c Config) applyDefaults() Config {
 	if c.DuplicateWriteBackStagger == 0 {
 		c.DuplicateWriteBackStagger = 30 * time.Minute
+	}
+	if c.DownloadTimeout == 0 {
+		c.DownloadTimeout = 5 * time.Minute
+	}
+	if c.UploadTimeout == 0 {
+		c.UploadTimeout = 10 * time.Minute
+	}
+	if c.ReplicationTimeout == 0 {
+		c.ReplicationTimeout = 3 * time.Minute
+	}
+	if c.BackendTimeout == 0 {
+		c.BackendTimeout = 2 * time.Minute
+	}
+	if c.ReadinessTimeout == 0 {
+		c.ReadinessTimeout = 30 * time.Second
+	}
+	if c.MaxConcurrentDownloads == 0 {
+		c.MaxConcurrentDownloads = 10
+	}
+	if c.MaxConcurrentUploads == 0 {
+		c.MaxConcurrentUploads = 5
+	}
+	if c.MaxRequestSize == 0 {
+		c.MaxRequestSize = 1024 * 1024 * 1024 // 1GB
+	}
+	if c.MaxRetries == 0 {
+		c.MaxRetries = 3
+	}
+	if c.RetryDelay == 0 {
+		c.RetryDelay = 100 * time.Millisecond
+	}
+	if c.RetryMaxDelay == 0 {
+		c.RetryMaxDelay = 5 * time.Second
 	}
 	return c
 }
