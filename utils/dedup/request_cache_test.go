b1116ed5ef673b6fd9e71b181a70aadef4202f44
akalpakchiev <99887781+Anton-Kalpakchiev@users.noreply.github.com>
2026-01-20 14:58:01 +0100

    Improve observability on kraken-origin load (#533)

--- utils/dedup/request_cache_test.go
+++ utils/dedup/request_cache_test.go
@@ -18,6 +18,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/uber-go/tally"
 	"github.com/uber/kraken/utils/testutil"
 
 	"github.com/andres-erbsen/clock"
@@ -35,7 +36,7 @@ func noop() error {
 func TestRequestCacheStartPreventsDuplicates(t *testing.T) {
 	require := require.New(t)
 
-	d := NewRequestCache(RequestCacheConfig{}, clock.New())
+	d := NewRequestCache(RequestCacheConfig{}, clock.New(), tally.NoopScope)
 
 	id := "foo"
 
@@ -46,7 +47,7 @@ func TestRequestCacheStartPreventsDuplicates(t *testing.T) {
 func TestRequestCacheStartClearsPendingWhenFuncDone(t *testing.T) {
 	require := require.New(t)
 
-	d := NewRequestCache(RequestCacheConfig{}, clock.New())
+	d := NewRequestCache(RequestCacheConfig{}, clock.New(), tally.NoopScope)
 
 	id := "foo"
 
@@ -60,7 +61,7 @@ func TestRequestCacheCachesErrors(t *testing.T) {
 	require := require.New(t)
 
 	clk := clock.NewMock()
-	d := NewRequestCache(RequestCacheConfig{}, clk)
+	d := NewRequestCache(RequestCacheConfig{}, clk, tally.NoopScope)
 
 	id := "foo"
 	err := errors.New("some error")
@@ -78,7 +79,7 @@ func TestRequestCacheExpiresErrors(t *testing.T) {
 		ErrorTTL: 5 * time.Second,
 	}
 	clk := clock.NewMock()
-	d := NewRequestCache(config, clk)
+	d := NewRequestCache(config, clk, tally.NoopScope)
 
 	id := "foo"
 	err := errors.New("some error")
@@ -101,7 +102,7 @@ func TestRequestCacheExpiresNotFoundErrorsIndependently(t *testing.T) {
 		NotFoundTTL: 30 * time.Second,
 	}
 	clk := clock.NewMock()
-	d := NewRequestCache(config, clk)
+	d := NewRequestCache(config, clk, tally.NoopScope)
 
 	id := "foo"
 	errNotFound := errors.New("error not found")
@@ -129,7 +130,7 @@ func TestRequestCacheStartCleansUpCachedErrors(t *testing.T) {
 		CleanupInterval: 10 * time.Second,
 	}
 	clk := clock.NewMock()
-	d := NewRequestCache(config, clk)
+	d := NewRequestCache(config, clk, tally.NoopScope)
 
 	err := errors.New("some error")
 
@@ -160,17 +161,67 @@ func TestRequestCacheLimitsNumberOfWorkers(t *testing.T) {
 		NumWorkers:  1,
 		BusyTimeout: 100 * time.Millisecond,
 	}
-	d := NewRequestCache(config, clock.New())
+	d := NewRequestCache(config, clock.New(), tally.NoopScope)
 
-	exit := make(chan bool)
+	exitCh := make(chan bool)
 
 	require.NoError(d.Start("a", func() error {
-		<-exit
+		<-exitCh
 		return nil
 	}))
 	require.Equal(ErrWorkersBusy, d.Start("b", noop))
 
 	// After a's function exits, the worker should be released.
-	exit <- true
+	exitCh <- true
 	require.NoError(d.Start("b", noop))
+}
+
+func TestRequestCacheEmitsNumRequests(t *testing.T) {
+	require := require.New(t)
+
+	testStats := tally.NewTestScope("", map[string]string{
+		"module": "test",
+	})
+	assertNumRequestsMetricValue := func(wantNumRequests float64) {
+		require.Eventually(func() bool {
+			numRequestsGauge, ok := testStats.Snapshot().Gauges()["num_requests+module=dedup"]
+			if !ok {
+				return false
+			}
+			return wantNumRequests == numRequestsGauge.Value()
+		}, time.Second, 10*time.Millisecond)
+	}
+
+	d := NewRequestCache(RequestCacheConfig{}, clock.New(), testStats)
+
+	exitCh := make(chan bool)
+	startRequest := func(requestID string) {
+		require.NoError(d.Start(requestID, func() error {
+			<-exitCh
+			return nil
+		}))
+	}
+	finishRequest := func() {
+		exitCh <- true
+	}
+
+	assertNumRequestsMetricValue(0)
+
+	startRequest("a")
+	assertNumRequestsMetricValue(1)
+
+	startRequest("b")
+	assertNumRequestsMetricValue(2)
+
+	finishRequest()
+	assertNumRequestsMetricValue(1)
+
+	startRequest("c")
+	assertNumRequestsMetricValue(2)
+
+	finishRequest()
+	assertNumRequestsMetricValue(1)
+
+	finishRequest()
+	assertNumRequestsMetricValue(0)
 }
