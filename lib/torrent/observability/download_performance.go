4c220721cc76a8dcb5eb373c90dfa5bc1f19b6fa
akalpakchiev <99887781+Anton-Kalpakchiev@users.noreply.github.com>
2026-04-16 21:14:13 +0200

    feat(agent): tune bucket ranges for download performance histograms (#600)

--- lib/torrent/observability/download_performance.go
+++ lib/torrent/observability/download_performance.go
@@ -21,11 +21,11 @@ import (
 )
 
 const (
-	_xsmall, _small, _medium, _large, _xlarge, _xxlarge = "0B-100MiB", "100MiB-1GB", "1GiB-2GiB", "2GiB-5GiB", "5GiB-10GiB", "10GiB+"
+	_xsmall, _small, _medium, _large, _xlarge, _xxlarge = "0B-5MiB", "5MiB-100MiB", "100MiB-1GB", "1GiB-5GiB", "5GiB-10GiB", "10GiB+"
 )
 
 var (
-	_sizeBoundaries = []uint64{0, 100 * memsize.MB, memsize.GB, 2 * memsize.GB, 5 * memsize.GB, 10 * memsize.GB}
+	_sizeBoundaries = []uint64{0, 5 * memsize.MB, 100 * memsize.MB, memsize.GB, 5 * memsize.GB, 10 * memsize.GB}
 	_sizeTags       = []string{_xsmall, _small, _medium, _large, _xlarge, _xxlarge}
 
 	_downloadLatencyBuckets tally.DurationBuckets
