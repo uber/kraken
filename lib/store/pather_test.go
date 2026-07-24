12f12b7dd9de54859214c31285a266ef0148b997
Anton Kalpakchiev <akalpakchiev@gmail.com>
2026-07-24 16:28:07 +0200

    feat(DiskStore): Make dir-sharding configurable
    Build-index currently does not use directory sharding. It also does not
    store blobs, whose keys are blob digests (i.e. SHA256 strings).
    Therefore, to replace the existing SimpleStore used by build-index, we
    need to make sure sharding is configurable. This commit makes that
    change.

--- lib/store/pather_test.go
+++ lib/store/pather_test.go
@@ -13,35 +13,43 @@ func TestPather(t *testing.T) {
 	rootDir, err := os.MkdirTemp("/tmp", "kraken-disk-store")
 	require.NoError(t, err)
 	t.Cleanup(func() { _ = os.RemoveAll(rootDir) })
-	pather := newPather(rootDir)
+	shardLength := 2
+	shardedPather := newPather(rootDir, shardLength)
+	unshardedPather := newPather(rootDir, 0)
 	md := metadata.NewTorrentMeta(core.MetaInfoFixture())
 	key := "8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78"
 
-	t.Run("incomplete entry", func(t *testing.T) {
+	t.Run("incomplete entry, sharded", func(t *testing.T) {
 		require := require.New(t)
-		complete := false
-		dirPath := pather.dirPath(key, complete)
-		wantDirPath := rootDir + "/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78"
-		require.Equal(wantDirPath, dirPath)
-		blobPath := pather.blobPath(key, complete)
-		wantBlobPath := rootDir + "/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data"
-		require.Equal(wantBlobPath, blobPath)
-		sidecarFilePath := pather.sidecarFilePath(key, complete, md.GetSuffix())
-		wantSidecarFilePath := rootDir + "/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta"
-		require.Equal(wantSidecarFilePath, sidecarFilePath)
+		require.Equal(rootDir+"/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78", shardedPather.dirPath(key, _incompleteBlob))
+		require.Equal(rootDir+"/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data", shardedPather.blobPath(key, _incompleteBlob))
+		require.Equal(rootDir+"/incomplete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta", shardedPather.sidecarFilePath(key, _incompleteBlob, md.GetSuffix()))
+	})
+	t.Run("incomplete entry, unsharded", func(t *testing.T) {
+		require := require.New(t)
+		require.Equal(rootDir+"/incomplete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78", unshardedPather.dirPath(key, _incompleteBlob))
+		require.Equal(rootDir+"/incomplete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data", unshardedPather.blobPath(key, _incompleteBlob))
+		require.Equal(rootDir+"/incomplete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta", unshardedPather.sidecarFilePath(key, _incompleteBlob, md.GetSuffix()))
 	})
 
-	t.Run("complete entry", func(t *testing.T) {
+	t.Run("complete entry, sharded", func(t *testing.T) {
 		require := require.New(t)
-		complete := true
-		dirPath := pather.dirPath(key, complete)
-		wantDirPath := rootDir + "/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78"
-		require.Equal(wantDirPath, dirPath)
-		blobPath := pather.blobPath(key, complete)
-		wantBlobPath := rootDir + "/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data"
-		require.Equal(wantBlobPath, blobPath)
-		sidecarFilePath := pather.sidecarFilePath(key, complete, md.GetSuffix())
-		wantSidecarFilePath := rootDir + "/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta"
-		require.Equal(wantSidecarFilePath, sidecarFilePath)
+		require.Equal(rootDir+"/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78", shardedPather.dirPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data", shardedPather.blobPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/8c/6a/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta", shardedPather.sidecarFilePath(key, _completeBlob, md.GetSuffix()))
+	})
+	t.Run("complete entry, unsharded", func(t *testing.T) {
+		require := require.New(t)
+		require.Equal(rootDir+"/complete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78", unshardedPather.dirPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/data", unshardedPather.blobPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/8c6af6ca6458353bfa8cb3d756ca54a4fe7b1de04196bf1b37e0863c3f806a78/_torrentmeta", unshardedPather.sidecarFilePath(key, _completeBlob, md.GetSuffix()))
+	})
+
+	t.Run("unsharded entries with non-digest keys", func(t *testing.T) {
+		key := "foo"
+		require := require.New(t)
+		require.Equal(rootDir+"/complete/foo", unshardedPather.dirPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/foo/data", unshardedPather.blobPath(key, _completeBlob))
+		require.Equal(rootDir+"/complete/foo/_torrentmeta", unshardedPather.sidecarFilePath(key, _completeBlob, md.GetSuffix()))
 	})
 }
