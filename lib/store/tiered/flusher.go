fd45b1308a22e211bc417c07be02f06d7bc6d877
Anton Kalpakchiev <akalpakchiev@gmail.com>
2026-07-31 20:00:51 +0200

    feat(tiered.Store): implement async flushing
    Next commit will implement the store itself. The flusher's job is to
    asynchronously flush data from the memory store to disk. I tried
    implementing it in a way where the muteces contend as little as possible
    and we don't block store APIs on disk IO, but it became a little complex
    so I'm open on any feedback on how to simplify the flusher.

---
+++ lib/store/tiered/flusher.go
@@ -0,0 +1,317 @@
+package tiered
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/uber/kraken/lib/store/disk"
+	"github.com/uber/kraken/lib/store/memory"
+	"github.com/uber/kraken/lib/store/metadata"
+	"github.com/uber/kraken/utils/closers"
+	"go.uber.org/zap"
+)
+
+type flusher struct {
+	blobs  map[string]*blob
+	queue  []string
+	mu     sync.Mutex
+	notify chan struct{}
+	stop   chan struct{}
+
+	mem  *memory.Store
+	disk *disk.Store
+	log  *zap.SugaredLogger
+}
+
+type blob struct {
+	key       string
+	dataDirty bool
+	dataSize  uint64
+	dirtyMD   map[string]struct{}
+	mu        sync.Mutex
+}
+
+func newFlusher(mem *memory.Store, disk *disk.Store, log *zap.SugaredLogger, numWorkers int) *flusher {
+	f := &flusher{
+		blobs:  make(map[string]*blob, 0),
+		queue:  make([]string, 0),
+		notify: make(chan struct{}, numWorkers),
+		stop:   make(chan struct{}),
+		mem:    mem,
+		disk:   disk,
+		log:    log,
+	}
+
+	for range numWorkers {
+		go f.worker()
+	}
+	return f
+}
+
+// Enqueues a blob and its metadata for flushing to disk. Should be called once per blob, upon MarkComplete.
+// Once a blob has been flushed, UnbanEviction is called on the mem store.
+func (f *flusher) markDirty(key string, sizeBytes uint64) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	dirtyMD, err := f.mem.ListMetadata(key)
+	if err != nil {
+		f.log.With(
+			"key", key,
+			"error", fmt.Errorf("mem store list metadata: %w", err),
+		).Error("Could not mark blob as dirty, aborting flushing")
+		_ = f.mem.UnbanEviction(key) // prevent leak
+		return
+	}
+	dirtyMDMap := make(map[string]struct{})
+	for _, dirtyMD := range dirtyMD {
+		dirtyMDMap[dirtyMD.GetSuffix()] = struct{}{}
+	}
+
+	f.blobs[key] = &blob{
+		key:       key,
+		dataDirty: true,
+		dataSize:  sizeBytes,
+		dirtyMD:   dirtyMDMap,
+	}
+	f.queue = append(f.queue, key)
+
+	select {
+	case f.notify <- struct{}{}:
+	default:
+	}
+}
+
+// Must be called on each md mutation.
+func (f *flusher) markMetadataDirty(key, mdSuffix string) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	b, dirty := f.blobs[key]
+	if dirty {
+		b.mu.Lock()
+		defer b.mu.Unlock()
+
+		b.dirtyMD[mdSuffix] = struct{}{}
+		return
+	}
+
+	_, onDisk := f.disk.Has(key)
+	if !onDisk {
+		return // Blob is not yet complete, we can't start flushing.
+	}
+
+	f.blobs[key] = &blob{
+		key:       key,
+		dataDirty: false,
+		dirtyMD:   map[string]struct{}{mdSuffix: {}},
+	}
+	f.queue = append(f.queue, key)
+
+	select {
+	case f.notify <- struct{}{}:
+	default:
+	}
+}
+
+// Aborts the flushing of a blob, possibly midway through. May leave corrupt state on disk, which caller is responsible for cleaning.
+func (f *flusher) abort(key string) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	delete(f.blobs, key)
+}
+
+// Stops all flushing gracefully.
+func (f *flusher) close() {
+	// TODO - consider whether there's a race when 1) a blob's data is flushed on disk, but the entry is in mem too, 2) new md is enqueued for flushing,
+	// 3) close is called, and 4) the workers exit before flushing the metadata, thus the disk blob is corrupt (blob data is persisted, but not metadata).
+	close(f.stop)
+}
+
+func (f *flusher) worker() {
+	for {
+		select {
+		case <-f.stop:
+			return
+		case <-f.notify:
+			for {
+				b, ok := f.nextToFlush()
+				if !ok {
+					break
+				}
+				f.flush(b)
+			}
+		}
+	}
+}
+
+func (f *flusher) nextToFlush() (b *blob, ok bool) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	for len(f.queue) > 0 {
+		key := f.queue[0]
+		f.queue = f.queue[1:]
+		b, ok := f.blobs[key]
+		if !ok {
+			continue
+		}
+		return b, true
+	}
+	return nil, false
+}
+
+func (f *flusher) flush(b *blob) {
+	key := b.key
+	defer f.mem.UnbanEviction(key)
+
+	if b.dataDirty {
+		if err := f.flushData(b); err != nil {
+			err = fmt.Errorf("flush data: %w", err)
+			f.log.With(
+				"key", key,
+				"error", err,
+			).Error("Could not flush data from mem to disk, abandoning flushing operation")
+			f.handleFlushFailure(key)
+			return
+		}
+	}
+
+	if err := f.flushMetadatasAndUnmarkDirty(key, b); err != nil {
+		err = fmt.Errorf("flush metadatas: %w", err)
+		f.log.With(
+			"key", key,
+			"error", err,
+		).Error("Could not flush metadata from mem to disk, abandoning flushing operation")
+		f.handleFlushFailure(key)
+	}
+}
+
+// Tries to prevent corrupt state in disk store upon failed flush. To do so, the flush to disk is aborted,
+// simulating similar behavior to the file being flushed and subsequently evicted by the disk store's LRU policy.
+// This will break any open [File] handles to the blob after eviction from memory, but it's the best we can do.
+func (f *flusher) handleFlushFailure(key string) {
+	if err := f.disk.Delete(key); err != nil && !errors.Is(err, os.ErrNotExist) {
+		f.log.With(
+			"key", key,
+			"error", err).
+			Error("Could not clean disk entry after flushing failed, blob is now leaked in disk store")
+	}
+
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	delete(f.blobs, key)
+}
+
+func (f *flusher) flushMetadatasAndUnmarkDirty(key string, b *blob) error {
+	b.mu.Lock()
+	for {
+		dirtyMDSnapshot := b.dirtyMD
+		b.dirtyMD = make(map[string]struct{})
+		b.mu.Unlock()
+
+		for mdSuffix := range dirtyMDSnapshot {
+			err := f.flushMetadata(key, mdSuffix)
+			if err != nil {
+				f.log.With(
+					"key", key,
+					"mdSuffix", mdSuffix,
+					"error", err,
+				).Error("Could not flush metadata from mem to disk")
+				return fmt.Errorf("flush md: %w", err)
+			}
+		}
+
+		f.mu.Lock()
+		b.mu.Lock()
+		if len(b.dirtyMD) == 0 {
+			delete(f.blobs, key)
+			b.mu.Unlock()
+			f.mu.Unlock()
+			return nil
+		}
+
+		f.mu.Unlock()
+	}
+}
+
+func (f *flusher) flushMetadata(key, mdSuffix string) error {
+	md := metadata.CreateFromSuffix(mdSuffix)
+	ok, err := f.mem.GetMetadata(key, md)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("mem store get md: %w", err)
+	}
+	if !ok {
+		err = f.disk.DeleteMetadata(key, md)
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		if err != nil {
+			return fmt.Errorf("disk store delete md: %w", err)
+		}
+		return nil
+	}
+	err = f.disk.SetMetadata(key, md)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("disk store set md: %w", err)
+	}
+
+	return nil
+}
+
+func (f *flusher) flushData(b *blob) error {
+	key := b.key
+	memF, err := f.mem.Open(key)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("mem store open: %w", err)
+	}
+	defer closers.Close(memF)
+	diskF, err := f.disk.Create(key, b.dataSize)
+	if err != nil {
+		return fmt.Errorf("disk store create: %w", err)
+	}
+	defer closers.Close(diskF)
+	f.mu.Lock()
+	_, ok := f.blobs[b.key]
+	if !ok {
+		// abort was called before we created the file, we need to cleanup.
+		err := f.disk.Delete(key)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			f.log.With(
+				"key", key,
+				"error", err).
+				Error("Could not clean disk entry after flushing failed, blob is now leaked in disk store")
+		}
+		f.mu.Unlock()
+		return nil
+	}
+	f.mu.Unlock()
+	_, err = io.Copy(diskF, memF)
+	if errors.Is(err, memory.ErrEvicted) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("io copy from mem file to disk file: %w", err)
+	}
+	err = f.disk.MarkComplete(key)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("disk mark complete: %w", err)
+	}
+	return nil
+}
